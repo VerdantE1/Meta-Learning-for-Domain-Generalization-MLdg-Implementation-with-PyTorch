d64bab524bd9b2e218ce9fed0d3f0f6d62fc8198
Frank <2523877046@qq.com>
2025-02-11 18:51:16 +0800

    final

--- MLDG.py
+++ MLDG.py
@@ -4,53 +4,31 @@ import numpy as np
 from torch import nn, optim
 from torch.utils.data import Dataset
 from utils import transform_to_8x9
-from model import DynamicGCN_DNN
+from model import DGCNN
 from sklearn.model_selection import train_test_split
 import os
 from scipy import io as scio
 from scipy.stats import zscore
+from utils import set_seed
 from torch.utils.data import DataLoader, TensorDataset
+
 
 # 设置设备
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-def load_DE_SEED(load_path):
-    """加载SEED数据集中的差分熵(DE)特征"""
-    datasets = scio.loadmat(load_path)
-    DE = datasets['DE']
-    dataAll = np.transpose(DE, [1, 0, 2])  # [样本数, 通道数, 特征数]
-    labelAll = datasets['labelAll'].flatten()
-    labelAll = labelAll + 1  # 标签从0开始，需要+1
-    return dataAll, labelAll
-
-
-class EEGTaskDataset(Dataset):
-    """用于处理单个 EEG 任务的数据集"""
-    def __init__(self, data, labels):
-        self.data = data
-        self.labels = labels
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        return (
-            torch.tensor(self.data[idx], dtype=torch.float32),
-            torch.tensor(self.labels[idx], dtype=torch.long),
-        )
-
-
-
-def meta_train_mldg(meta_model, meta_optimizer, train_tasks, test_task, num_iterations,
+def meta_train_mldg(meta_model, meta_optimizer, all_tasks, num_iterations,
                     inner_lr, inner_steps, beta,num_meta_val_domains,eval_interval):
     print("开始 MLDG 元学习训练...")
 
     query_acc_history = []  # 记录 meta-train 的准确率
     eval_results = []       # 记录评估结果 (test_loss 和 test_acc)
+
+    ## 初步设置train_task和test_task
+    train_tasks = all_tasks[:-1]
+    test_task = all_tasks[-1]
+
     best_acc = 0
-
-
     for iteration in range(num_iterations):
         meta_optimizer.zero_grad()
 
@@ -85,7 +63,7 @@ def meta_train_mldg(meta_model, meta_optimizer, train_tasks, test_task, num_iter
         grads = torch.autograd.grad(meta_train_loss, meta_model.parameters(), create_graph=True)
 
 
-
+        print(1)
 
 
         # updated_params一定是tensor,故是传播中的一个结点。在这里构建updated_params到param的计算图
@@ -162,44 +140,15 @@ def meta_train_mldg(meta_model, meta_optimizer, train_tasks, test_task, num_iter
 
         # 打印详细的调试信息
         print(f"Iteration {iteration + 1}/{num_iterations}:")
-        #print(f"  Meta-Train Loss = {meta_train_loss.item():.4f}")
-        #print(f"  Meta-Val Loss = {meta_val_loss.item():.4f}")
-        #print(f"  Total Loss = {total_loss.item()}")
-        #print(f"  Meta-Train Grad Norm = {train_grad_norm:.4f}")
-        #print(f"  Meta-Val Grad Norm (beta-scaled) = {val_grad_norm:.4f}")
+        print(f"  Meta-Train Loss = {meta_train_loss.item():.4f}")
+        print(f"  Meta-Val Loss = {meta_val_loss.item():.4f}")
+        print(f"  Total Loss = {total_loss.item():.4f}")
+        print(f"  Meta-Train Grad Norm = {train_grad_norm:.4f}")
+        print(f"  Meta-Val Grad Norm (beta-scaled) = {val_grad_norm:.4f}")
         print(f"  Meta-Train Accuracy = {query_acc * 100:.2f}%")
         print(f"  Meta-Train Best_Test_Accuracy = {best_acc * 100:.2f}%")
 
     print("MLDG 元学习训练完成！")
-    return best_acc
-
-def evaluate_meta(meta_model, test_data, test_labels):
-    """
-    在测试数据上直接评估模型性能，无需微调。
-    """
-    print("开始元测试...")
-    cnt = 0
-    # 转换为张量并移动到设备
-    test_data = torch.tensor(test_data, dtype=torch.float32).to(device)
-    test_labels = torch.tensor(test_labels, dtype=torch.long).to(device)
-
-    # 测试阶段直接评估模型
-    with torch.no_grad():  # 确保评估阶段不计算梯度
-        test_pred = meta_model(test_data)
-        test_loss = nn.CrossEntropyLoss()(test_pred, test_labels)
-        test_acc = (test_pred.argmax(dim=1) == test_labels).float().mean().item()
-
-
-    #print(f"Iteration {cnt + 1}/{cnt }:")
-    print(f"Test Loss: {test_loss.item():.4f}")
-    print(f"Test Accuracy: {test_acc * 100:.2f}%")
-
-    cnt += 1
-    return test_loss.item(), test_acc
-
-
-
-
 
 
 def meta_train_mldg_loso(all_tasks, num_iterations, inner_lr, inner_steps, beta, num_meta_val_domains, eval_interval):
@@ -254,53 +203,3 @@ def meta_train_mldg_loso(all_tasks, num_iterations, inner_lr, inner_steps, beta,
 
 
 
-
-def main():
-    # 加载数据
-    data_dir = r'E:/SEED/SEED_Divide/DE/session1/'
-    file_list = os.listdir(data_dir)
-
-    # 设置超参数
-    set_seed(520)
-    num_iterations = 800  # 元训练迭代次数
-    batch_size = 64         # 批量大小
-    inner_lr = 0.0004        # 内循环学习率
-    meta_step_size = 0.005  # 元学习步长
-    weight_decay = 0.0004   # 权重衰减
-    meta_val_beta = 1   # meta-val 损失的权重
-    inner_steps = 1          # 内循环优化步数
-    num_meta_val_domains = 3 # 虚拟测试V个数
-    eval_interval = 15
-
-    # 构建任务数据集
-    all_tasks = []
-    for filename in file_list:  # 使用部分被试作为训练任务
-        file_path = os.path.join(data_dir, filename)
-        data, labels = load_DE_SEED(file_path)
-
-
-        if len(data) > 1:  # 确保数据足够
-            # 同步随机打乱 data 和 labels
-            indices = np.random.permutation(len(data))  # 随机生成索引
-            data = data[indices]  # 按索引打乱数据
-            labels = labels[indices]  # 按相同索引打乱标签
-
-            all_tasks.append((data,labels))
-
-
-    loso_results = meta_train_mldg_loso(
-        all_tasks=all_tasks,
-        num_iterations=num_iterations,
-        inner_lr=inner_lr,
-        inner_steps=inner_steps,
-        beta=meta_val_beta,
-        num_meta_val_domains=num_meta_val_domains,
-        eval_interval=eval_interval
-    )
-
-
-
-
-
-if __name__ == "__main__":
-    main()
